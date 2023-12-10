e05aec79db1c0be8ba5d84226646075d5a386ad2
anneandersen92 <anne_92@live.no>
2023-12-10 02:22:31 +0100

    project slider 90% complete
    dynamic link icons color???

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
 import { projectsData } from "./data/projects.js";
 
+// const projectsSlider = document.querySelector("#projects-slider")
+const sliderBtn = document.querySelector(".slider-btn")
 const projects = document.querySelector(".projects")
 
 for (let i = 0; i < projectsData.length; i++) {
     const project = projectsData[i];
 
-    createProjectHtml(project);
-}
+    createProjectCardHtml(project);
+    createSlideBtnHtml(project);
+};
 
-function createProjectHtml(project) {
-    projects
+function createProjectCardHtml(project) {
     projects.innerHTML +=
-        `<div class="project-card" style="background-color: ${project.backgroundColor}; color: ${project.textColor}">
+        `<div id="${project.id}" class="project-card" style="background-color: ${project.backgroundColor}; color: ${project.textColor}">
             <h2>${project.title}</h2>
             <h3>${project.assignment}</h3>
-            <img src="${project.img}" alt="${project.imgAlt}">
-            <a href="${project.url}" class="netlify">Website</a>
-            <a href="${project.githubUrl}" class="git-hub">GitHub</a>
+            <div class="links">
+                <a href="${project.url}" class="netlify"><img src="../resources/icons/website_icon.png"></a>
+                <a href="${project.githubUrl}" class="git-hub"><img src="../resources/icons/github_icon.png"></a>
+            </div>
+            <img class="project-img" src="${project.img}" alt="${project.imgAlt}">
         </div>`;
-}
+};
+
+function createSlideBtnHtml(project) {
+    sliderBtn.innerHTML += `<a href="#${project.id}">${project.id}</a>`
+};
